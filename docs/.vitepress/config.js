856b352ca12d556ab7f9604c74b6d3a38e3bf701
CassiopeiaCode <i@5i.gs>
2025-07-02 21:21:41 +0800

    Automated deployment of frontend on 2025-07-02 21:21:41

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -6,6 +6,17 @@ export default defineConfig({
   title: "NekoForces",
   description: "An AI-driven competitive programming solution collector.",
   base: '/',
+  markdown: {
+    // By changing the default Vue template delimiters, we prevent VitePress
+    // from misinterpreting text like `{{1, 2}}` as a Vue expression.
+    vue: {
+      template: {
+        compilerOptions: {
+          delimiters: ['((%', '%))']
+        }
+      }
+    }
+  },
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
